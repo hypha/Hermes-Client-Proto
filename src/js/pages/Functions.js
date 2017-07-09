77f2fb3a123bfc1e12537b7310772995fb58c76c
hypha <ramble.dalloway@gmail.com>
2017-07-09 20:25:31 +0100

    functions page now works using react data grid:x

--- src/js/pages/Functions.js
+++ src/js/pages/Functions.js
@@ -2,29 +2,15 @@ import React from "react";
 
 import Article from "../components/Article";
 
+import FunctionsGrid from "../components/FunctionsGrid";
+
+
 export default class Functions extends React.Component {
   render() {
-    const { query } = this.props.location;
-    const { params } = this.props;
-    const { article } = params;
-    const { date, filter } = query;
-
-    const Articles = [
-      "Some Article",
-      "Some Other Article",
-      "Yet Another Article",
-      "Still More",
-      "Fake Article",
-      "Partial Article",
-      "American Article",
-      "Mexican Article",
-    ].map((title, i) => <Article key={i} title={title}/> );
-
     return (
       <div>
-        <h1>Functions</h1>
-        article: {article}, date: {date}, filter: {filter}
-        <div class="row">{Articles}</div>
+        <h1>Functions > User </h1>
+        <div class="container"> <FunctionsGrid /> </div>
       </div>
     );
   }
