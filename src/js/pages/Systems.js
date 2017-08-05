2f787366657a3f1c032b0927ba07b38db97be7da
hypha <ramble.dalloway@gmail.com>
2017-08-05 18:53:38 +0100

    added a rest call demo in system page

--- src/js/pages/Systems.js
+++ src/js/pages/Systems.js
@@ -1,10 +1,29 @@
 import React from "react";
 
-export default class Systems extends React.Component {
+export default class ItemLister extends React.Component {
+	constructor() {
+  	super();
+ 		 this.state={items:[]};
+  }
+  componentDidMount(){
+  	fetch(`http://jsonplaceholder.typicode.com/posts`)
+ 		.then(result=>result.json())
+    .then(items=>this.setState({items}))
+  }
   render() {
     console.log("systems");
-    return (
-      <h1>Systems</h1>
-    );
+  	return(
+      <div>
+        <h1>Systems </h1>
+
+      	<ul>
+            {this.state.items.length ?
+            	this.state.items.map(item=><li key={item.id}>{item.title}</li>)
+              : <li>Loading...</li>
+            }
+        </ul>
+
+      </div>
+   )
   }
 }
